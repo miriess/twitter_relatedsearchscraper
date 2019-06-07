7af62abf084397620d2cb69adaed9ed7545a97df
Michael Riess <micha.riess@gmail.com>
2019-06-07 16:57:07 +0200

    added documentation for module/script and class

--- twitter_rss.py
+++ twitter_rss.py
@@ -1,3 +1,24 @@
+"""Twitter Related Search Scraper
+
+This script scrapes Twitter for the related searches of a query and continues
+with the gathered new queries until the specified depth of search is reached.
+
+The first command line argument is a string that is the start of the search.
+Following this arguments can be added to
+    * specify the depth of the search (as integer)
+    * make the script export a gephi-readable csv -> add "gephi"
+    * make the script export a csv of search terms and queries -> add "output"
+    * make the script export a csv of only the search terms
+        -> add "results" in addition to "output"
+These additions can be made in any order. If multiple integers are given, the
+script runs on the smallest.
+
+The script requires the packages pandas, csv, sys, requests, bs4, random, time.
+
+This file can also be imported as a module and contains the class(es):
+    * related_search_hub
+"""
+
 import pandas as pd
 import csv
 import sys
@@ -7,6 +28,56 @@ import random
 from time import sleep
 
 class related_search_hub:
+    """
+    A class used to represent one scraping instance.
+
+    ...
+
+    Atributes
+    ---------
+    initial_search : str
+        A string with the search term to start the scraping with.
+    initial_query : str
+        A string containing the formatted query to be added to the twitter url.
+    depth_to_go : int
+        The depth to which the subsearches are executed. Starts at a given value
+        and is reduced while descending.
+    level : int
+        Interger denoting the level to which the search was already executed.
+    standard_wait : float
+        Floating point number denoting the wait time between requests to
+        twitter. Default value is random between .25 and .75 seconds.
+    error_list : list
+        List to track the errors while accessing twitter. Starts empty and is
+        filled when running the script - reported at the end.
+    results_df : pd.DataFrame
+        DataFrame that stores the results of the searches as well as future
+        search queries. Columns are
+        type|level|search|result_1|result_2|...|result_5
+
+    Methods
+    -------
+    get_related_searches(query)
+        Access twitter and gets the related searches connected to the provided
+        query string. Returns a tuple of the result and query list.
+    get_results(query=False, search=False)
+        Uses get_related to access twitter and stores results in results_df.
+    go_deeper()
+        Automatedly conducts all the searches in the current level, increases
+        level, decreases depth_to_go, and writes all results into results_df.
+    results_as_csv(only_results=False)
+        Exports results_df as csv in current directory. If only_results is True
+        omits the search querie rows.
+    export_gephi_csv()
+        Exports results_df as gephi readable csv. Import to gephi as undirected
+        graph with method sum for multiple edge occurrences.
+    full_descent(with_output=False, only_results_output=False, with_gephi=False)
+        Automatedly conducts all the searches until the specified depth is
+        reached. Arguments specify the type of csv output.
+            * with_output uses results_as_csc and passes only_results_output on.
+            * with_gephi uses export_gephi_csv.
+    """
+
     def __init__(self, start_search, depth_to_go=1, wait_time=random.uniform(.25,.75)):
         self.initial_search = start_search
         self.standard_wait = wait_time
@@ -55,7 +126,7 @@ class related_search_hub:
                     sleep(self.standard_wait)
         print(f'----- finished level {self.level} -----')
 
-    def get_related_searches(self, query, waittime=random.uniform(.25,.75)):
+    def get_related_searches(self, query):
         r = requests.get('http://www.twitter.com' + query)
         similar_results_bucket = BeautifulSoup(r.text, 'html.parser').find_all('ul', class_="AdaptiveRelatedSearches-items")
         if (similar_results_bucket and (similar_results_bucket[0].find_all('a', class_='js-nav'))):
@@ -80,6 +151,7 @@ class related_search_hub:
 
     def export_gephi_csv(self):
         search_string = self.initial_search.replace(" ", "_")
+        print('----- saving csv for gephi -----')
         self.results_df.loc[self.results_df.type=='result','search':].to_csv(
             f'gephi_{search_string}_depth{self.level}.csv',
             index=False,
